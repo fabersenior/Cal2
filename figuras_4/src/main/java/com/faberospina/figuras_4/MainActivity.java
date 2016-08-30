ebd483fcadafeb95efa271d2ca3e801ce079e266
Faber <faber_729@hotmail.com>
2016-08-30 16:46:07 -0500

    Practica_2 Version 5.0

--- figuras_4/src/main/java/com/faberospina/figuras_4/MainActivity.java
+++ figuras_4/src/main/java/com/faberospina/figuras_4/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     EditText lado,base,altura,radio ;
     TextView resultado;
     RadioGroup group;
+    int sel=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,33 +45,49 @@ public class MainActivity extends AppCompatActivity {
         switch(view.getId()) {
             case R.id.Rcuadro:
                 if (checked){
+                    lado.setText("");
                     lado.setVisibility(View.VISIBLE);
+                    sel=1;
+                }else {
+                    sel=0;
                 }
                // lado.setVisibility(View.INVISIBLE);
                     // Pirates are the best
                     break;
             case R.id.Rtri:
                 if (checked){
+                    base.setText("");
+                    altura.setText("");
                     base.setVisibility(View.VISIBLE);
                     altura.setVisibility(View.VISIBLE);
+                    sel=2;
 
+                }else{
+                    sel=0;
                 }
 
                     break;
 
             case R.id.Rcircle:
                 if (checked){
+                    radio.setText("");
                     radio.setVisibility(View.VISIBLE);
-
+                    sel=3;
+                }else{
+                    sel=0;
                 }
 
                 break;
 
             case R.id.Rsquare:
                 if (checked){
+                    lado.setText("");
+                    base.setText("");
                     lado.setVisibility(View.VISIBLE);
                     base.setVisibility(View.VISIBLE);
-
+                    sel=4;
+                }else{
+                    sel=0;
                 }
 
                 break;
@@ -85,38 +102,41 @@ public class MainActivity extends AppCompatActivity {
 
     public  void onButtonClicked(View view){
 
-        if(lado.length()==0 ){
+        if(lado.length()==0 && sel==1){
             //resultado.setText("Campos Vacios");
             Toast.makeText(this,"Campo de Lado Vacio"
                     , Toast.LENGTH_SHORT).show();
-        }else {
+        }else if(sel==1){
+
             int n1 = Integer.parseInt(lado.getText().toString());
             resultado.setText("El area del cuadrado es: " + Integer.toString(n1*n1));
         }
 
-        if(base.length()==0 || altura.length()==0){
+        if((base.length()==0 || altura.length()==0) && sel==2){
             //resultado.setText("Campos Vacios");
             Toast.makeText(this,"Campo de base o altura Vacio"
                     , Toast.LENGTH_SHORT).show();
-        }else {
+        }else if(sel==2){
             int n1 = Integer.parseInt(base.getText().toString());
             int n2 = Integer.parseInt((altura.getText().toString()));
             resultado.setText("El area del Triangulo es: " + Double.toString((double)(n1*n2)/2 ));
         }
-        if(radio.length()==0 ){
+        if(radio.length()==0 && sel==3 ){
             //resultado.setText("Campos Vacios");
             Toast.makeText(this,"Campo de Radio Vacio"
                     , Toast.LENGTH_SHORT).show();
-        }else {
-            int n1 = Integer.parseInt(base.getText().toString());
+        }else if(sel==3) {
+
+            int n1 = Integer.parseInt(radio.getText().toString());
             resultado.setText("El area del Triangulo es: " + Double.toString((double)(2*Math.PI*n1*n1 )));
         }
 
-        if(lado.length()==0 || base.length()==0 ){
+        if(lado.length()==0 || base.length()==0 && sel ==4){
             //resultado.setText("Campos Vacios");
             Toast.makeText(this,"Campo de lado Vacio"
                     , Toast.LENGTH_SHORT).show();
-        }else {
+        }else if(sel==4){
+
             int n1 = Integer.parseInt(base.getText().toString());
             int n2 = Integer.parseInt(lado.getText().toString());
             resultado.setText("El area del Rectangulo es: " + Double.toString((double)(n1*n2 )));
